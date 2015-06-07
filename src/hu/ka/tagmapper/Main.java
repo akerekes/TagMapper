ecd750afaf8ca3c3f5b9168bd0aec49d7739d99c
andras.kerekes <andras.kerekes.work@gmail.com>
2015-06-07 12:24:54 -0400

    process files concurrently

--- src/hu/ka/tagmapper/Main.java
+++ src/hu/ka/tagmapper/Main.java
@@ -1,17 +1,5 @@
 package hu.ka.tagmapper;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.StringTokenizer;
 import java.util.logging.Logger;
 
 public class Main {
@@ -19,7 +7,11 @@ public class Main {
 	private static Logger LOGGER = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
-		new TagMapper().processPath(args[0]);
+		if (args.length != 1) {
+			LOGGER.severe("Single command line argument is path to a file or directory to search for tags.");
+		} else {
+			new TagMapper().processPath(args[0]);
+		}
 	}
 
 }
--- src/hu/ka/tagmapper/TagMapper.java
+++ src/hu/ka/tagmapper/TagMapper.java
@@ -1,25 +1,39 @@
 package hu.ka.tagmapper;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 import hu.ka.tagmapper.util.StacktracePrinter;
 
 public class TagMapper {
 
+	private static final int TIMEOUT_VALUE = 30;
+	private static final TimeUnit TIMEOUT_UNIT = TimeUnit.SECONDS;
 	private static Logger LOGGER = Logger.getLogger(TagMapper.class.getName());
+
+	private static class ExecutionContext<V> {
+		ExecutorCompletionService<V> executor = new ExecutorCompletionService<>(Executors.newWorkStealingPool());
+		List<Future<V>> futures = new LinkedList<>();
+		void submit(Callable<V> callable) {
+			futures.add(executor.submit(callable));
+		}
+	}
 
 	public void processPath(String path) {
 		try {
@@ -28,7 +42,8 @@ public class TagMapper {
 			if (fileOrDir.isFile()) {
 				filesToKws = new TagExtractor(fileOrDir).call();
 			} else {
-				filesToKws = processDir(fileOrDir);
+				ExecutionContext executionContext = processDir(fileOrDir, new ExecutionContext<>());
+				filesToKws = collectTags(executionContext);
 			}
 			LOGGER.info("Collected file->keyword mapping: " + filesToKws.toString());
 			Map<String, Set<String>> kwToFiles = new HashMap<>();
@@ -43,25 +58,43 @@ public class TagMapper {
 		}
 	}
 
-	private Map<String, Set<String>> processDir(File fileOrDir) {
+	private Map<String, Set<String>> collectTags(ExecutionContext<Map<String, Set<String>>> executionContext) {
+		Map<String, Set<String>> result = new HashMap<>();
+		try {
+			for (int i = 0; i < executionContext.futures.size(); i++) {
+
+				Future<Map<String, Set<String>>> future = executionContext.executor.poll(TIMEOUT_VALUE, TIMEOUT_UNIT);
+				if (future != null) {
+					if (future.isDone()) {
+						try {
+							result.putAll(future.get());
+						} catch (ExecutionException e) {
+							LOGGER.severe(new StacktracePrinter(e));
+						}
+					} else {
+						LOGGER.warning("Computation was not completed successfully, status: " + (future.isCancelled() ? "cancelled" : "error"));
+					}
+				} else {
+					LOGGER.severe("Processing timed out");
+				}
+			}
+		} catch (InterruptedException e) {
+			LOGGER.info(new StacktracePrinter(e));
+		}
+		return result;
+	}
+
+	private ExecutionContext processDir(File fileOrDir, ExecutionContext<Map<String, Set<String>>> executionContext) {
 		DirectoryStream<Path> paths;
-		final Map<String, Set<String>> fileToKw = new HashMap<>();
 		try {
 			paths = Files.newDirectoryStream(fileOrDir.toPath());
 			paths.forEach(path -> {
 				try {
-					Map<String, Set<String>> kws;
 					if (path.toFile().isFile()) {
-						kws = new TagExtractor(path.toFile()).call();
+						executionContext.submit(new TagExtractor(path.toFile()));
 					} else {
-						kws = processDir(path.toFile());
+						processDir(path.toFile(), executionContext);
 					}
-					kws.forEach((s, strings) ->
-							fileToKw.merge(s, strings, (a, b) ->
-							{
-								a.addAll(b);
-								return a;
-							}));
 				} catch (Exception e) {
 					LOGGER.severe(new StacktracePrinter(e));
 				}
@@ -69,7 +102,7 @@ public class TagMapper {
 		} catch (Exception e) {
 			LOGGER.severe(new StacktracePrinter(e));
 		}
-		return fileToKw;
+		return executionContext;
 	}
 
 }

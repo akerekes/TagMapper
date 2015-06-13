7a1b3968cdbef70b6d6ced2d7514ea0aab5f90cb
andras.kerekes <andras.kerekes.work@gmail.com>
2015-06-13 19:53:18 -0400

    Copy and modify TextUtils.extractText to allow for separator between extracted text items.

--- src/main/java/hu/ka/documentprocessor/DocxParser.java
+++ src/main/java/hu/ka/documentprocessor/DocxParser.java
@@ -1,13 +1,21 @@
 package hu.ka.documentprocessor;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.StringWriter;
+import java.io.Writer;
 
-import org.docx4j.TextUtils;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import org.docx4j.jaxb.Context;
+import org.docx4j.jaxb.NamespacePrefixMapperUtils;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 public class DocxParser {
 
@@ -23,7 +31,7 @@ public class DocxParser {
 		try {
 			WordprocessingMLPackage doc = WordprocessingMLPackage.load(docxFile);
 			StringWriter text = new StringWriter();
-			TextUtils.extractText(doc.getMainDocumentPart().getContents(), text);
+			extract(doc.getMainDocumentPart().getContents(), text);
 			return text.toString();
 		} catch (Docx4JException e) {
 			LOGGER.error("Could not load file: " + filepath, e);
@@ -32,4 +40,36 @@ public class DocxParser {
 		}
 		return "";
 	}
+
+	private void extract(Object o, Writer w) throws JAXBException {
+		Marshaller marshaller= Context.jc.createMarshaller();
+		NamespacePrefixMapperUtils.setProperty(marshaller,
+				NamespacePrefixMapperUtils.getPrefixMapper());
+		marshaller.marshal(o, new TextExtractor(w, " "));
+	}
+
+	static class TextExtractor extends DefaultHandler {
+
+		private Writer out;
+		private String separator;
+
+		public TextExtractor(Writer out, String separator) {
+			this.out = out;
+			this.separator = separator;
+		}
+
+		public void characters(char[] text, int start, int length)
+				throws SAXException {
+
+			try {
+				out.write(text, start, length);
+				out.write(separator);
+			}
+			catch (IOException e) {
+				throw new SAXException(e);
+			}
+
+		}
+
+	} // end TextExtractor
 }
